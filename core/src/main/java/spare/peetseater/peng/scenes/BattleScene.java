e8b15759c46b60418166fee8c104df6e5252ee28
peetseater <peetseater@gmail.com>
2025-09-07 19:15:18 -0400

    Mock out what the scene will look like

--- core/src/main/java/spare/peetseater/peng/scenes/BattleScene.java
+++ core/src/main/java/spare/peetseater/peng/scenes/BattleScene.java
@@ -1,8 +1,10 @@
 package spare.peetseater.peng.scenes;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
@@ -27,6 +29,9 @@ public class BattleScene implements Scene {
 
         assets = new LinkedList<>();
         assets.add(GameAssets.scoreFont);
+        assets.add(GameAssets.redPaddle);
+        assets.add(GameAssets.bluePaddle);
+        assets.add(GameAssets.ball);
     }
 
     @Override
@@ -42,11 +47,18 @@ public class BattleScene implements Scene {
         font.draw(
             gameRunner.batch,
             String.format("%d | %d\n", p1Score, p2Score),
-            0f, Gdx.graphics.getHeight() - font.getLineHeight(),
+            0f, Gdx.graphics.getHeight() - font.getLineHeight()/2f,
             Gdx.graphics.getWidth(),
             Align.center,
             false
         );
+        Texture p1 = gameRunner.assets.getTexture(GameAssets.redPaddle);
+        Texture p2 = gameRunner.assets.getTexture(GameAssets.bluePaddle);
+        Texture ball = gameRunner.assets.getTexture(GameAssets.ball);
+        gameRunner.batch.draw(p1, 0, Gdx.graphics.getHeight() - font.getLineHeight() * 1.5f, Gdx.graphics.getWidth(), 1);
+        gameRunner.batch.draw(p1, 18, 50, 18, 72);
+        gameRunner.batch.draw(p2, Gdx.graphics.getWidth() - 18 - 18, 150, 18, 72);
+        gameRunner.batch.draw(ball, Gdx.graphics.getWidth() / 2f, 500, 18, 18);
     }
 
     @Override
