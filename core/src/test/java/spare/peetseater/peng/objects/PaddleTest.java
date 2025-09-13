34d6809293c4c19b5d94b06e84a4068ef971b5f5
peetseater <peetseater@gmail.com>
2025-09-13 02:07:15 -0400

    Add some more tests for paddle bounce angle
    Just wanting to make sure that the 0 in the y isn't doing any
    hiding of interesting behavior or something

--- core/src/test/java/spare/peetseater/peng/objects/PaddleTest.java
+++ core/src/test/java/spare/peetseater/peng/objects/PaddleTest.java
@@ -21,4 +21,20 @@ class PaddleTest {
         float radians = right.getBounceAngle(ball);
         assertEquals(MathUtils.degreesToRadians * 270, radians);
     }
+
+    @Test
+    void test90DegreesLeftPaddleHigherOnBoard() {
+        Paddle left = new Paddle(10,60);
+        Ball ball = new Ball(10, 60 + Paddle.HEIGHT/2);
+        float radians = left.getBounceAngle(ball);
+        assertEquals(MathUtils.degreesToRadians * 90, radians);
+    }
+
+    @Test
+    void test90DegreesRightPaddleHigherOnBoard() {
+        Paddle right = new Paddle(100, 100);
+        Ball ball = new Ball(100 - Ball.CIRCUMFERENCE, 100 + Paddle.HEIGHT/2);
+        float radians = right.getBounceAngle(ball);
+        assertEquals(MathUtils.degreesToRadians * 270, radians);
+    }
 }
