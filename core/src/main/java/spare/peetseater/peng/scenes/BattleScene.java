db720028aad689951cf1ff48c64ae12987f3a5b1
peetseater <peetseater@gmail.com>
2025-09-13 22:53:15 -0400

    Handle collisions + draw borders

--- core/src/main/java/spare/peetseater/peng/scenes/BattleScene.java
+++ core/src/main/java/spare/peetseater/peng/scenes/BattleScene.java
@@ -1,13 +1,11 @@
 package spare.peetseater.peng.scenes;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Align;
@@ -29,30 +27,33 @@ public class BattleScene implements Scene {
     Paddle red;
     Paddle blue;
     Ball ball;
-    int p1Score;
-    int p2Score;
+    int redScore;
+    int blueScore;
     List<AssetDescriptor<?>> assets;
 
     public BattleScene(GameRunner gameRunner) {
         this.gameRunner = gameRunner;
-        p1Score = p2Score = 0;
+        redScore = blueScore = 0;
 
         red = new Paddle(18, VIRTUAL_HEIGHT / 2f);
         blue = new Paddle(VIRTUAL_WIDTH - Paddle.WIDTH * 2, VIRTUAL_HEIGHT / 2f);
-        ball = new Ball(VIRTUAL_WIDTH/2f, VIRTUAL_HEIGHT/2f);
-        ball.setVelocity(new Vector2(MathUtils.random() * VIRTUAL_WIDTH * 0.5f, MathUtils.random() * VIRTUAL_HEIGHT * 0.6f));
+        resetBall();
 
         assets = new LinkedList<>();
         assets.add(GameAssets.scoreFont);
         assets.add(GameAssets.redPaddle);
         assets.add(GameAssets.bluePaddle);
+        assets.add(GameAssets.wall);
         assets.add(GameAssets.ball);
+    }
+
+    private void resetBall() {
+        ball = new Ball(VIRTUAL_WIDTH/2f, VIRTUAL_HEIGHT/2f);
+        ball.setVelocity(new Vector2(MathUtils.random() * VIRTUAL_WIDTH * 0.5f, MathUtils.random() * VIRTUAL_HEIGHT * 0.6f));
     }
 
     @Override
     public void update(float delta) {
-        p1Score += 1;
-        p2Score -= 1;
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
             red.moveUp(delta);
         }
@@ -71,23 +72,22 @@ public class BattleScene implements Scene {
         if (blue.intersects(ball)) {
             ball.bounceOffOf(blue);
         }
-        if (ball.getAnchorX() < 0 || (ball.getAnchorX() + ball.CIRCUMFERENCE) > VIRTUAL_WIDTH ) {
-            ball = new Ball(VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2);
-            ball.setVelocity(new Vector2(- Ball.INITIAL_SPEED, 0f));
+
+        // bounce ball off non scoring walls
+        if (ball.getAnchorY() <= 0) {
+            ball.bounceUp();
         }
-        if (ball.getAnchorY() < 0 || (ball.getAnchorY() + ball.CIRCUMFERENCE) > VIRTUAL_HEIGHT ) {
-            ball = new Ball(VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2);
-            ball.setVelocity(new Vector2(- Ball.INITIAL_SPEED, 0f));
+        if (ball.getAnchorY() + Ball.CIRCUMFERENCE >= VIRTUAL_HEIGHT) {
+            ball.bounceDown();
         }
-//        if (top.intersects(ball) || bottom.intersect(ball)) {
-//            // bounce ball off non scoring walls
-//        }
-//        if (ball.toTheLeftOf(red)) {
-//            // Score a point for blue.
-//        }
-//        if (ball.toTheRightOf(blue)) {
-//            // Score a point for red
-//        }
+        if (ball.toTheLeftOf(red)) {
+            blueScore += 1;
+            resetBall();
+        }
+        if (ball.toTheRightOf(blue)) {
+            redScore += 1;
+            resetBall();
+        }
         ball.update(delta);
     }
 
@@ -97,7 +97,7 @@ public class BattleScene implements Scene {
         BitmapFont font = gameRunner.assets.getFont(GameAssets.scoreFont);
         font.draw(
             gameRunner.batch,
-            String.format("%d | %d\n", p1Score, p2Score),
+            String.format("%d   %d\n", redScore, blueScore),
             0f, Gdx.graphics.getHeight() - font.getLineHeight()/2f,
             Gdx.graphics.getWidth(),
             Align.center,
@@ -106,10 +106,21 @@ public class BattleScene implements Scene {
         Texture redTexture = gameRunner.assets.getTexture(GameAssets.redPaddle);
         Texture blueTexture = gameRunner.assets.getTexture(GameAssets.bluePaddle);
         Texture ballTexture = gameRunner.assets.getTexture(GameAssets.ball);
+        Texture wallTexture = gameRunner.assets.getTexture(GameAssets.wall);
 
+        for (int i = 0; i < VIRTUAL_WIDTH / Ball.CIRCUMFERENCE; i++) {
+            int x = i * (int)Ball.CIRCUMFERENCE;
+            gameRunner.batch.draw(wallTexture, x, 0, Ball.CIRCUMFERENCE, Ball.RADIUS);
+            gameRunner.batch.draw(wallTexture, x, VIRTUAL_HEIGHT - Ball.RADIUS, Ball.CIRCUMFERENCE, Ball.RADIUS);
+        }
+        for (int i = 0; i < VIRTUAL_HEIGHT / Ball.CIRCUMFERENCE; i++) {
+            int y = i * (int)Ball.CIRCUMFERENCE;
+            gameRunner.batch.draw(wallTexture, VIRTUAL_WIDTH/2, y, Ball.CIRCUMFERENCE, Ball.CIRCUMFERENCE);
+        }
+
+        gameRunner.batch.draw(ballTexture, ball.getAnchorX(), ball.getAnchorY(), Ball.CIRCUMFERENCE, Ball.CIRCUMFERENCE);
         gameRunner.batch.draw(redTexture, red.getX(), red.getY(), Paddle.WIDTH, Paddle.HEIGHT);
         gameRunner.batch.draw(blueTexture, blue.getX(), blue.getY(), Paddle.WIDTH, Paddle.HEIGHT);
-        gameRunner.batch.draw(ballTexture, ball.getAnchorX(), ball.getAnchorY(), Ball.CIRCUMFERENCE, Ball.CIRCUMFERENCE);
     }
 
     @Override
